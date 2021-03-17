6ad0211269c16fcf33971eebfb65dbe74edbeed1
Tsatsu Kofi Amable <tsatsu.amable@gmail.com>
2021-03-17 19:42:51 +0000

    Added Spring starter web with a hello world controller

--- src/main/java/com/locode/bulksms/controllers/Helloworld.java
+++ src/main/java/com/locode/bulksms/controllers/Helloworld.java
@@ -1,4 +1,13 @@
 package com.locode.bulksms.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Helloworld {
+
+    @GetMapping("/")
+    public String getGreeting(){
+        return "Hello Worlds";
+    }
 }
